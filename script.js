e52d1bd99a1d4c092230715b9ba9abb77dbc403a
MahmoudSalah2022 <103763804+MahmoudSalah2022@users.noreply.github.com>
2023-03-18 11:03:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function nextcard() {
+    let submit = document.querySelector('.submit');
+    const fristcont = document.querySelector('.fristcont');
+    const secondcard = document.querySelector('.secondcont');
+    fristcont.classList.add('hiddensection');
+    secondcard.classList.remove('hiddensection');
+}
